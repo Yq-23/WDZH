f1cbf4f3151317d9debcc8c98c5bbdd53f6b8219
Yangqing <873165527@qq.com>
2020-09-21 21:02:52 +0800

    篮球计分程序

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -9,55 +9,63 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity{
 
     //private static final String TAG = "MainActivity";
-    TextView out;
+    TextView outA;
+    TextView outB;
     //EditText inp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        //获取控件 out
-        TextView outobj = findViewById(R.id.out);
-        outobj.setText("Hello");
-        Log.i(TAG, "onCreate:...... ");
-        EditText inp = findViewById(R.id.inp);
-        //String text = inp.getText().toString();
-        //Button btn = findViewById(R.id.btn);*/
-         out = (TextView)findViewById(R.id.out);
-         //inp = (EditText)findViewById(R.id.inp);
+         outA = (TextView)findViewById(R.id.outA);
+         outB = (TextView)findViewById(R.id.outB);
     }
-    /*public void btn(View v){
-        String str = inp.getText().toString();
-        Double a = Double.parseDouble(str);
-        Double b = a*1.8 + 32.0;
-        String str1 = String.valueOf(b);
-        out.setText("结果是：" + str1);
-    }*/
     public void btn1(View v){
-        String str = out.getText().toString();
+        String str = outA.getText().toString();
         int a = Integer.parseInt(str);
         int b = a + 3;
         String str1 = String.valueOf(b);
-        out.setText(str1);
+        outA.setText(str1);
     }
     public void btn2(View v){
-        String str = out.getText().toString();
+        String str = outA.getText().toString();
         int a = Integer.parseInt(str);
         int b = a + 2;
         String str1 = String.valueOf(b);
-        out.setText(str1);
+        outA.setText(str1);
     }
     public void btn3(View v){
-        String str = out.getText().toString();
+        String str = outA.getText().toString();
         int a = Integer.parseInt(str);
         int b = a + 1;
         String str1 = String.valueOf(b);
-        out.setText(str1);
+        outA.setText(str1);
     }
     public void btn4(View v){
+        String str = outB.getText().toString();
+        int a = Integer.parseInt(str);
+        int b = a + 3;
+        String str1 = String.valueOf(b);
+        outB.setText(str1);
+    }
+    public void btn5(View v){
+        String str = outB.getText().toString();
+        int a = Integer.parseInt(str);
+        int b = a + 2;
+        String str1 = String.valueOf(b);
+        outB.setText(str1);
+    }
+    public void btn6(View v){
+        String str = outB.getText().toString();
+        int a = Integer.parseInt(str);
+        int b = a + 1;
+        String str1 = String.valueOf(b);
+        outB.setText(str1);
+    }
+    public void btn7(View v){
         int a = 0;
         String str1 = String.valueOf(a);
-        out.setText(str1);
+        outA.setText(str1);
+        outB.setText(str1);
     }
 }
