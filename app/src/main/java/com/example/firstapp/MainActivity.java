6d07142f0172a52278db4850e047071d6471c5f8
Yangqing <873165527@qq.com>
2020-09-23 09:08:00 +0800

    汇率转换（第一个页面）及汇率修改（第二个页面），汇率修改后传回第一个页面按新的汇率重新计算

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,70 +1,117 @@
 package com.example.firstapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
-    TextView outA;
-    TextView outB;
-    //EditText inp;
+    private static final String TAG = "MainActivity";
+    TextView out;
+    EditText inp;
+    double dollarRate = 0.1471;
+    double euroRate = 0.1251;
+    double wonRate = 171.3427;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         outA = (TextView)findViewById(R.id.outA);
-         outB = (TextView)findViewById(R.id.outB);
+
+        out = (TextView)findViewById(R.id.out);
+        inp = (EditText)findViewById(R.id.inp);
+
     }
-    public void btn1(View v){
-        String str = outA.getText().toString();
-        int a = Integer.parseInt(str);
-        int b = a + 3;
-        String str1 = String.valueOf(b);
-        outA.setText(str1);
+
+    public void reversion(View btn){
+        String str = inp.getText().toString();
+        if(str==null || str.equals("") || str.equals(R.string.hint)){
+            //no input
+            Toast.makeText(MainActivity.this, "请输入金额", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            double a = Double.parseDouble(str);
+            if(btn.getId()==R.id.btn_dollar){
+                double b = a*dollarRate;
+                String str1 = String.format("%.4f", b).toString();
+                out.setText(str1+" dollar");
+            }
+            else if(btn.getId()==R.id.btn_euro){
+                double b = a*euroRate;
+                String str1 = String.format("%.4f", b).toString();
+                out.setText(str1+" euro");
+            }
+            else if(btn.getId()==R.id.btn_won){
+                double b = a*wonRate;
+                String str1 = String.format("%.4f", b).toString();
+                out.setText(str1+" won");
+            }
+        }
     }
-    public void btn2(View v){
-        String str = outA.getText().toString();
-        int a = Integer.parseInt(str);
-        int b = a + 2;
-        String str1 = String.valueOf(b);
-        outA.setText(str1);
+
+    public void open(View V){
+        Intent second = new Intent(this,MainActivity2.class);
+
+        //使用Extra传递参数
+        /*second.putExtra("dollar_rate_key",dollarRate);
+        second.putExtra("euro_rate_key",euroRate);
+        second.putExtra("won_rate_key",wonRate);*/
+
+        //使用Bundle传递参数
+        Bundle bdl = new Bundle();
+        bdl.putDouble("dollar_rate_key",dollarRate);
+        bdl.putDouble("euro_rate_key",euroRate);
+        bdl.putDouble("won_rate_key",wonRate);
+        second.putExtras(bdl);
+
+        Log.i(TAG,"onCreate:dollarRate=" + dollarRate);
+        Log.i(TAG,"onCreate:euroRate=" + euroRate);
+        Log.i(TAG,"onCreate:wonRate=" + wonRate);
+
+        startActivityForResult(second,1);
     }
-    public void btn3(View v){
-        String str = outA.getText().toString();
-        int a = Integer.parseInt(str);
-        int b = a + 1;
-        String str1 = String.valueOf(b);
-        outA.setText(str1);
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode==1 && resultCode==1){
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getDouble("key_dollar",0.1f);
+            euroRate = bundle.getDouble("key_euro",0.1f);
+            wonRate = bundle.getDouble("key_won",0.1f);
+
+            /*dollarRate = data.getDoubleExtra("key_dollar",0.1f);
+            euroRate = data.getDoubleExtra("key_euro",0.1f);
+            wonRate = data.getDoubleExtra("key_won",0.1f);*/
+
+            Log.i(TAG,"onActivityResult: dollarRate=" + dollarRate);
+            Log.i(TAG,"onActivityResult: euroRate=" + euroRate);
+            Log.i(TAG,"onActivityResult: wonRate=" + wonRate);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
-    public void btn4(View v){
-        String str = outB.getText().toString();
-        int a = Integer.parseInt(str);
-        int b = a + 3;
-        String str1 = String.valueOf(b);
-        outB.setText(str1);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.first_menu,menu);
+        return true;
     }
-    public void btn5(View v){
-        String str = outB.getText().toString();
-        int a = Integer.parseInt(str);
-        int b = a + 2;
-        String str1 = String.valueOf(b);
-        outB.setText(str1);
-    }
-    public void btn6(View v){
-        String str = outB.getText().toString();
-        int a = Integer.parseInt(str);
-        int b = a + 1;
-        String str1 = String.valueOf(b);
-        outB.setText(str1);
-    }
-    public void btn7(View v){
-        int a = 0;
-        String str1 = String.valueOf(a);
-        outA.setText(str1);
-        outB.setText(str1);
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu1){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/firstapp/MainActivity2.java
@@ -0,0 +1,86 @@
+package com.example.firstapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity2";
+    EditText txt_dollar,txt_euro,txt_won;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+
+        //使用extra获得数据
+        /*double dollar2 = intent.getDoubleExtra("dollar_rate_key",0.0f);
+        double euro2 = intent.getDoubleExtra("euro_rate_key",0.0f);
+        double won2 = intent.getDoubleExtra("won_rate_key",0.0f);*/
+
+        //使用bundle获得数据
+        Bundle bundle = intent.getExtras();
+        double dollar2 = bundle.getDouble("dollar_rate_key",0.1f);
+        double euro2 = bundle.getDouble("euro_rate_key",0.1f);
+        double won2 = bundle.getDouble("won_rate_key",0.1f);
+
+        Log.i(TAG,"onCreate:dollar2=" + dollar2);
+        Log.i(TAG,"onCreate:euro2=" + euro2);
+        Log.i(TAG,"onCreate:won2=" + won2);
+
+        txt_dollar = (EditText)findViewById(R.id.txt_dollar);
+        txt_euro = (EditText)findViewById(R.id.txt_euro);
+        txt_won = (EditText)findViewById(R.id.txt_won);
+
+        txt_dollar.setText(String.valueOf(dollar2));
+        txt_euro.setText(String.valueOf(euro2));
+        txt_won.setText(String.valueOf(won2));
+    }
+
+    public void btn_save(View btn){
+        if(btn.getId()==R.id.btn_save){
+            Intent intent_save = getIntent();
+            Bundle bdl = new Bundle();
+            double newDollar = Double.parseDouble(txt_dollar.getText().toString());
+            double newEuro = Double.parseDouble(txt_euro.getText().toString());
+            double newWon = Double.parseDouble(txt_won.getText().toString());
+
+            /*intent_save.putExtra("key_dollar",newDollar);
+            intent_save.putExtra("key_euro",newEuro);
+            intent_save.putExtra("key_won",newWon);*/
+
+            bdl.putDouble("key_dollar",newDollar);
+            bdl.putDouble("key_euro",newEuro);
+            bdl.putDouble("key_won",newWon);
+            intent_save.putExtras(bdl);
+
+            //设置resultCode及带回的数据
+            setResult(1,intent_save);
+            //返回到调用页面
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.first_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu1){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
